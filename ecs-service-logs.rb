18a49895e07b34eb0f3e26cfb6544ad03ab8c130
trussworks-infra <infra+github@truss.works>
2020-03-20 13:24:40 -0700

    Brew formula update for ecs-service-logs version v0.1.3

--- ecs-service-logs.rb
+++ ecs-service-logs.rb
@@ -2,16 +2,16 @@
 class EcsServiceLogs < Formula
   desc "ecs-service-logs is used to filter JSON-formatted log lines in CloudWatch."
   homepage "https://github.com/trussworks/ecs-service-logs"
-  version "0.1.2"
+  version "0.1.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/trussworks/ecs-service-logs/releases/download/v0.1.2/ecs-service-logs_0.1.2_Darwin_x86_64.tar.gz"
-    sha256 "0ec494508836e6a6397433297c9ec64969d31d4e499e0b75a82465b06c553829"
+    url "https://github.com/trussworks/ecs-service-logs/releases/download/v0.1.3/ecs-service-logs_0.1.3_Darwin_x86_64.tar.gz"
+    sha256 "b7e1c221f8e764f074c709caa894b100153606c69e9efcdc6254df40eaf48ebf"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/trussworks/ecs-service-logs/releases/download/v0.1.2/ecs-service-logs_0.1.2_Linux_x86_64.tar.gz"
-      sha256 "99ba22ee97dd58388337fb2a2cdee910dbb73a5d840e14ebb2b4b9d681278aed"
+      url "https://github.com/trussworks/ecs-service-logs/releases/download/v0.1.3/ecs-service-logs_0.1.3_Linux_x86_64.tar.gz"
+      sha256 "a65aa2255b0e30cf45eb2d5030e27f29b466b42c6cdefe080e0beebbbfef8363"
     end
   end
 
